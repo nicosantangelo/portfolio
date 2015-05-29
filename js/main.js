c39c8884256ba09abf6d2ec4b4aa4134c28b9331
Nicolás Santángelo <santangelonicolas@gmail.com>
2015-05-29 01:14:43 -0300

    Add bxSlider

--- js/main.js
+++ js/main.js
@@ -1,15 +1,25 @@
-new WOW().init();
+(function() {
+    // Slider
+    var $slider = $('.bxslider');
+    $slider.bxSlider({
+        pager: false
+    });
 
-$('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
+    // Animate CSS on scroll
+    new WOW().init();
 
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-        if (target.length) {
-            $('html,body').animate({
-                scrollTop: target.offset().top
-            }, 1000);
-            return false;
+    // Animate anchor links
+    var $html = $('html,body');
+    $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $html.animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
         }
-    }
-});
+    });
+})();
