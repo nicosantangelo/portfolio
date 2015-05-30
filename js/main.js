92e32c9a4fde6b9c8c6e8542bf343d179198788c
Nicolás Santángelo <santangelonicolas@gmail.com>
2015-05-30 16:05:28 -0300

    Navbar

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
 (function() {
+    // Highlights the navigation menu items while scrolling.
+    $('body').scrollspy({
+        target: '.navbar-fixed-top',
+        offset: 51
+    });
+
     // Slider
     var $slider = $('.bxslider');
     $slider.bxSlider({
@@ -17,9 +23,31 @@
             if (target.length) {
                 $html.animate({
                     scrollTop: target.offset().top
-                }, 1000);
+                }, 800);
                 return false;
             }
+        }
+    });
+
+    // Easter
+    var $document = $(document);
+    var konamiKeys = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+    var konamiIndex = 0;
+    var corny = function(interval) {
+        cornify_add();
+        setTimeout(function() { corny(interval * 1.5); }, interval);
+    };
+    $document.on('keydown', function(e) {
+        if(e.keyCode === konamiKeys[konamiIndex++]) {
+            if(konamiIndex === konamiKeys.length) {
+                $document.off('keydown');
+                $.getScript('http://www.cornify.com/js/cornify.js',function() {
+                    corny(300);
+                    $document.on("keydown", cornify_add);
+                }); 
+            }
+        } else {
+            konamiIndex = 0;
         }
     });
 })();
