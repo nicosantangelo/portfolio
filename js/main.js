8945e9f0cd772d23253fd246f67327a526bf6f76
Nicolás Santángelo <santangelonicolas@gmail.com>
2016-09-17 09:26:11 -0300

    First redesign attempt using granim js

--- js/main.js
+++ js/main.js
@@ -12,32 +12,76 @@
         offset: 51
     });
 
+    var granim = new Granim({
+        element: "#logo-canvas",
+        direction: "left-right",
+        opacity: [1, 1],
+        isPausedWhenNotInView: true,
+        stateTransitionSpeed: 1000,
+        states: {
+            "default-state": {
+                gradients: [
+                    ["#00d2ff", "#3a7bd5"],
+                    ["#4776E6", "#8E54E9"],
+                    ["#000428", "#004e92"],
+                    ["#FF512F", "#DD2476"],
+                    ["#fd746c", "#ff9068"],
+                    ["#6a3093", "#a044ff"],
+                    ["#76b852", "#8DC26F"],
+                    ["#005C97", "#363795"]
+                ],
+                transitionSpeed: 3000 
+            },
+            "github": {
+                gradients: [['#000000', '#F5F5F5'], ['#F5F5F5', '#000000']],
+                transitionSpeed: 3000 
+            },
+            "twitter": {
+                gradients: [['#1DA1F3', '#66757F'], ['#66757F', '#1DA1F3']],
+                transitionSpeed: 3000 
+            },
+            "stackoverflow": {
+                gradients: [['#F48024', '#666666'], ['#666666', '#F48024']],
+                transitionSpeed: 3000 
+            },
+            "email": {
+                gradients: [['#D14836', '#F5F5F5'], ['#F5F5F5', '#D14836']],
+                transitionSpeed: 3000 
+            },
+        }
+    })
+
+    $("#logo").on("mouseenter", function() {
+        granim.changeState("email")
+    }).on("mouseleave", function() {
+        granim.changeState("default-state")
+    })
+
     // Slider
-    var $slider = $('.bxslider');
-    $slider.bxSlider({
-        pager: false
-    });
+    // var $slider = $('.bxslider');
+    // $slider.bxSlider({
+    //     pager: false
+    // });
 
     // Animate CSS on scroll
     new WOW().init();
 
     // Animate anchor links
-    var $html = $('html,body');
-    $('a[href*=#]:not([href=#])').click(function() {
-        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-            if (target.length) {
-                $html.animate({
-                    scrollTop: target.offset().top
-                }, 800);
-                return false;
-            }
-        }
-    });
+    // var $html = $('html,body');
+    // $('a[href*=#]:not([href=#])').click(function() {
+    //     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
+    //         var target = $(this.hash);
+    //         target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    //         if (target.length) {
+    //             $html.animate({
+    //                 scrollTop: target.offset().top
+    //             }, 800);
+    //             return false;
+    //         }
+    //     }
+    // });
 
     // Package downloads
-
     var packages = {}; // Pseudo set
     $(".js-downloads-count").each(function() {
         var packageName = $(this).data('packageName');
