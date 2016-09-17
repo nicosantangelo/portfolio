0919a4ac4b35db56df97329ccd6a0850355194bc
Nicolás Santángelo <santangelonicolas@gmail.com>
2016-09-17 10:33:44 -0300

    Completely different layout

--- js/main.js
+++ js/main.js
@@ -2,60 +2,60 @@
     "use strict";
 
     // Keep the background image from resizing when the scrollbar on mobile moves
-    var windowHeight = $(window).height();
-    $("#top").height(windowHeight);
-    $("#langy").height(windowHeight);
+    // var windowHeight = $(window).height();
+    // $("#top").height(windowHeight);
+    // $("#langy").height(windowHeight);
 
     // Highlights the navigation menu items while scrolling.
-    $('body').scrollspy({
-        target: '.navbar-fixed-top',
-        offset: 51
-    });
+    // $('body').scrollspy({
+    //     target: '.navbar-fixed-top',
+    //     offset: 51
+    // });
 
-    var granim = new Granim({
-        element: "#logo-canvas",
-        direction: "left-right",
-        opacity: [1, 1],
-        isPausedWhenNotInView: true,
-        stateTransitionSpeed: 1000,
-        states: {
-            "default-state": {
-                gradients: [
-                    ["#00d2ff", "#3a7bd5"],
-                    ["#4776E6", "#8E54E9"],
-                    ["#000428", "#004e92"],
-                    ["#FF512F", "#DD2476"],
-                    ["#fd746c", "#ff9068"],
-                    ["#6a3093", "#a044ff"],
-                    ["#76b852", "#8DC26F"],
-                    ["#005C97", "#363795"]
-                ],
-                transitionSpeed: 3000 
-            },
-            "github": {
-                gradients: [['#000000', '#F5F5F5'], ['#F5F5F5', '#000000']],
-                transitionSpeed: 3000 
-            },
-            "twitter": {
-                gradients: [['#1DA1F3', '#66757F'], ['#66757F', '#1DA1F3']],
-                transitionSpeed: 3000 
-            },
-            "stackoverflow": {
-                gradients: [['#F48024', '#666666'], ['#666666', '#F48024']],
-                transitionSpeed: 3000 
-            },
-            "email": {
-                gradients: [['#D14836', '#F5F5F5'], ['#F5F5F5', '#D14836']],
-                transitionSpeed: 3000 
-            },
-        }
-    })
+    // var granim = new Granim({
+    //     element: "#logo-canvas",
+    //     direction: "left-right",
+    //     opacity: [1, 1],
+    //     isPausedWhenNotInView: true,
+    //     stateTransitionSpeed: 1000,
+    //     states: {
+    //         "default-state": {
+    //             gradients: [
+    //                 ["#00d2ff", "#3a7bd5"],
+    //                 ["#4776E6", "#8E54E9"],
+    //                 ["#000428", "#004e92"],
+    //                 ["#FF512F", "#DD2476"],
+    //                 ["#fd746c", "#ff9068"],
+    //                 ["#6a3093", "#a044ff"],
+    //                 ["#76b852", "#8DC26F"],
+    //                 ["#005C97", "#363795"]
+    //             ],
+    //             transitionSpeed: 3000 
+    //         },
+    //         "github": {
+    //             gradients: [['#000000', '#F5F5F5'], ['#F5F5F5', '#000000']],
+    //             transitionSpeed: 3000 
+    //         },
+    //         "twitter": {
+    //             gradients: [['#1DA1F3', '#66757F'], ['#66757F', '#1DA1F3']],
+    //             transitionSpeed: 3000 
+    //         },
+    //         "stackoverflow": {
+    //             gradients: [['#F48024', '#666666'], ['#666666', '#F48024']],
+    //             transitionSpeed: 3000 
+    //         },
+    //         "email": {
+    //             gradients: [['#D14836', '#F5F5F5'], ['#F5F5F5', '#D14836']],
+    //             transitionSpeed: 3000 
+    //         },
+    //     }
+    // })
 
-    $("#logo").on("mouseenter", function() {
-        granim.changeState("email")
-    }).on("mouseleave", function() {
-        granim.changeState("default-state")
-    })
+    // $("#logo").on("mouseenter", function() {
+    //     granim.changeState("email")
+    // }).on("mouseleave", function() {
+    //     granim.changeState("default-state")
+    // })
 
     // Slider
     // var $slider = $('.bxslider');
@@ -64,7 +64,7 @@
     // });
 
     // Animate CSS on scroll
-    new WOW().init();
+    // new WOW().init();
 
     // Animate anchor links
     // var $html = $('html,body');
