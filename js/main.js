5bc391b8cf892450108f98a776e6c7d639606a1d
Nicolás Santángelo <santangelonicolas@gmail.com>
2016-09-17 13:33:56 -0300

    Iteration 1

--- js/main.js
+++ js/main.js
@@ -1,56 +1,6 @@
 (function() {
     "use strict";
-
-    var canvas = document.getElementById('logo-canvas')
-    var ctx = canvas.getContext("2d")
-    ctx.globalCompositeOperation = 'destination-over'
-
-    var granim = new Granim({
-        element: "#logo-canvas",
-        direction: "left-right",
-        opacity: [1, 1],
-        isPausedWhenNotInView: true,
-        stateTransitionSpeed: 1000,
-        states: {
-            'default-state': {
-                gradients: [
-                    ['#FFA500', '#E04B40'],
-                    ['#E04B40', '#FFA500'],
-                    ['#587EBF', '#0FA960'],
-                    ['#0FA960', '#587EBF'],
-                ],
-                transitionSpeed: 3000 
-            },
-            'sublime-text': {
-                gradients: [['#FFA500', '#E04B40'], ['#FFA500', '#6A3093'], ['#6A3093', '#E04B40']],
-                transitionSpeed: 3000 
-            },
-            'chrome-extensions': {
-                gradients: [['#FF512F', '#DD2476'], ['#000428', '#004E92']],
-                transitionSpeed: 3000 
-            },
-            'projects': {
-                gradients: [['#FD746C', '#FF9068'], ['#6A3093', '#A044FF'], ['#76B852', '#8DC26F']],
-                transitionSpeed: 3000 
-            },
-            'profiles': {
-                gradients: [['#F48024', '#666666'], ['#666666', '#F48024']],
-                gradients: [['#4776e6', '#8e54e9'], ['#000428', '#004E92'], ['#FF512F', '#DD2476']],
-                transitionSpeed: 3000 
-            }
-        }
-    });
-
-    $.each(['sublime-text', 'chrome-extensions', 'projects', 'profiles'], function(key) {
-        $('.' + key + ' .box').on('mouseenter', function() {
-            granim.changeState(key);
-        }).on('mouseleave', function() {
-            granim.changeState('default-state');
-        })
-    });
-
-    // Package downloads
-
+    
     var packages = ['Gulp', 'Jasmine JS', 'Trello', 'Copy from Find Results', 'I18n Rails', 'Jest']
     var promises = []
     var total    = 0
