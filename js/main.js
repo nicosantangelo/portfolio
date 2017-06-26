a028ba4e4e900f008e800b50381d58bc6dee3bd7
Nicolás Santángelo <santangelonicolas@gmail.com>
2017-06-27 00:24:32 +0200

    Use YQL to re-enable JSONP

--- js/main.js
+++ js/main.js
@@ -14,13 +14,25 @@
 
 
     $.each(packages, function(index, packageName) {
+        var url = 'https%3A%2F%2Fpackagecontrol.io%2Fpackages%2F' + encodeURIComponent(encodeURIComponent(packageName)) + '.json';
+        var query = 'SELECT * FROM json WHERE url="' + url + '"';
+
         var promise = $.ajax({
-            url: 'https://jsonp.afeld.me/?callback=?&url=https%3A%2F%2Fpackagecontrol.io%2Fpackages%2F' + encodeURIComponent(packageName) + '.json',
-            dataType: 'jsonp'
-        }).done(function(packageInfo) {
-            var $downloadCounters = $("[data-package-name='" + packageName + "']");
-            addDownloads($downloadCounters, packageInfo.installs.total);
-            total += packageInfo.installs.total;
+            url: 'http://query.yahooapis.com/v1/public/yql?q=' + query,
+            data: {
+                format: 'json',
+                jsonCompat: 'new',
+            },
+            dataType: 'jsonp',
+        }).done(function(data) {
+            var results = data.query && data.query.results
+            console.log(packageName, results)
+            if (results) {
+                var installs = results.json.installs
+                var $downloadCounters = $("[data-package-name='" + packageName + "']");
+                addDownloads($downloadCounters, installs.total);
+                total += installs.total;
+            }
         });
 
         promises.push(promise);
