884557a5ba4017e1f3b0e8359e63a422f83d458a
Nicolás Santángelo <santangelonicolas@gmail.com>
2025-05-05 00:52:14 -0300

    better navigation

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -10,17 +10,29 @@ export default function Navigation() {
   const currentLocation = usePathname();
   return (
     <ul className={styles.navigation}>
-      {currentLocation === locations.home() ? null : (
+      {currentLocation === locations.home() ? (
+        <li className={styles.selected}>
+          <span>Home</span>
+        </li>
+      ) : (
         <li>
           <Link href={locations.home()}>Home</Link>
         </li>
       )}
-      {currentLocation === locations.blog() ? null : (
+      {currentLocation.includes(locations.blog()) ? (
+        <li className={styles.selected}>
+          <span>Blog</span>
+        </li>
+      ) : (
         <li>
           <Link href={locations.blog()}>Blog</Link>
         </li>
       )}
-      {currentLocation === locations.links() ? null : (
+      {currentLocation === locations.links() ? (
+        <li className={styles.selected}>
+          <span>Links</span>
+        </li>
+      ) : (
         <li>
           <Link href={locations.links()}>Links</Link>
         </li>
